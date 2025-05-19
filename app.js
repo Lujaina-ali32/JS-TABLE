9b79f359ce03c3650b1df95308dd0c7edaa30c00
LUJAINA ALI <lujainaali10@gmail.com>
2025-05-20 00:02:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let table = prompt(" Enter Your Table: ");
+document.write(table , " X " , 1 , " = " , table*1 , "<br>");
+document.write(table , " X " , 2 , " = " , table*2 , "<br>");
+document.write(table , " X " , 3 , " = " , table*3 , "<br>");
+document.write(table , " X " , 4 , " = " , table*4 , "<br>");
+document.write(table , " X " , 5 , " = " , table*5 , "<br>");
+document.write(table , " X " , 6 , " = " , table*6 , "<br>");
+document.write(table , " X " , 7 , " = " , table*7 , "<br>");
+document.write(table , " X " , 8 , " = " , table*8 , "<br>");
+document.write(table , " X " , 9 , " = " , table*9 , "<br>");
+document.write(table , " X " , 10 , " = " , table*10 , "<br>");
